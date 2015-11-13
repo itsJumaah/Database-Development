cd36834f45dc8d104e1da6becd732f54df5be018
Buzz <buzz@Bilals-MacBook-Pro.local>
2015-11-13 14:23:44 +1300

    Database Development
    SQL

---
+++ insertion.sql
@@ -0,0 +1,83 @@
+
+INSERT INTO OFFICE VALUES 
+(0614,'8813995','drivealbany@zealandia.co.nz','23 Intel drive, Albany, Auckland','08615967');
+
+INSERT INTO OFFICE VALUES 
+(0628, '98581682','drivemassey@zealandia.co.nz', '9 Westgate drive, Massey, Auckland','12315990');
+
+INSERT INTO OFFICE VALUES
+(8213, '38492456','driveglenfield@zealandia.co.nz', '45 Hill Road, Glenfield, Auckland','45615978');
+
+INSERT INTO OFFICE VALUES
+(4232, '78945632', 'drivenc@zealandia.co.nz', '13 Lake Road, Northcote, Auckland','12915967');
+
+INSERT INTO OFFICE VALUES
+(4232, '97695234', 'drivehenderson@zealandia.co.nz', '38 Marshell street, Henderson, Auckland');
+
+
+INSERT INTO STAFF VALUES
+(08615967, 'Smith', 'John', 'k','Manager','14/06/1984','M','434 Waiwera way, Albany, Auckland','09876397','johnks@mailme.com','0614');
+
+INSERT INTO STAFF VALUES
+(08615678, 'James', 'Mark', 'G','AdminStaff','24/03/1989','M','56 Mikeway Road, Albany, Auckland','09762963','jamesgm@mailme.com','0614');
+
+INSERT INTO STAFF VALUES
+(08615097, 'Lang', 'Cindy','AdminStaff','12/09/1994','F','56 Lake Road, Northcote, Auckland','08752896','Cinlang@mailme.com','0614');
+
+INSERT INTO STAFF VALUES
+(08615567, 'Cho', 'jake', 'SeniorInstructor','09/11/1979','M','60 Judge way, Albany, Auckland','09641986','chojake@mailme.com','0614');
+
+INSERT INTO STAFF VALUES
+(08615789, 'singh', 'Jai', 'K','SeniorInstructor','19/05/1981','M','4 Era drive, Albany, Auckland','09875387','singhisKing@mailme.com','0614');
+
+
+
+INSERT INTO STAFF VALUES
+(12315990, 'Kumar', 'Ashneel', 'A','Manager','13/06/1994','M','96 Coronation way, Massey, Auckland','09986679','ashneelak@mailme.com','0628');
+
+INSERT INTO STAFF VALUES
+(12215967, 'smith', 'Jordan','AdminStaff','13/04/1993','M','65 Smudge way, Massey, Auckland','08532897','jsmith@mailme.com','0628');
+
+INSERT INTO STAFF VALUES
+(12315990, 'Kruger', 'Milan', 'A','AdminStaff','10/11/1991','M','289 Mac way, Massey, Auckland','08964297','ashneelak@mailme.com','0628');
+
+INSERT INTO STAFF VALUES
+(12215907, 'Burden', 'Andrew', 'B','SeniorInstructor','11/12/1997','M','87 Hewllet way, Massey, Auckland','078965347','Burdenba@mailme.com','0628');
+
+INSERT INTO STAFF VALUES
+(122645965, 'Park', 'Betty', 'L','SeniorInstructor','19/03/1987','F','76 Wayward road, Albany, Auckland','07856839','ParkB@mailme.com','0628');
+
+
+
+
+INSERT INTO STAFF VALUES
+(45615978, 'Kumar', 'John','Manager','13/07/1995','M','99 somenation street, Glenfield, Auckland','07896468','Jakek@mailme.com','8213');
+
+INSERT INTO STAFF VALUES
+(4565967, 'Prasad', 'Ashika','AdminStaff','10/08/1992','F','09 Anderson street, Glenfield, Auckland','07974278','ashikap@mailme.com','8213');
+
+INSERT INTO STAFF VALUES
+(45615667, 'Chang', 'Ben','AdminStaff','19/02/1986','M','21 Glen street, Glenfield, Auckland','07567843','ChangBen@mailme.com','8213');
+
+INSERT INTO STAFF VALUES
+(45682963, 'Perry', 'Katty','SeniorInstructor','10/12/1991','F','09 Firework street, Glenfield, Auckland','07745687','Kattyperry@mailme.com','8213');
+
+INSERT INTO STAFF VALUES
+(45615978, 'Sheran', 'Ed','SeniorInstructor','16/03/1989','M','14 Hobbiton street, Glenfield, Auckland','079735678','edsheran@mailme.com','8213');
+
+
+
+INSERT INTO STAFF VALUES
+(12915967, 'baggins', 'Biblo','Manager','13/08/1977','M','98 Bagends street, Northcote, Auckland','02579782','Bilblobag@mailme.com','4232');
+
+INSERT INTO STAFF VALUES
+(12987658, 'Potter', 'Harry','AdminStaff','17/01/1978','M','09 Hogwarts street, Northcote, Auckland','027865479','harryp@mailme.com','8213');
+
+INSERT INTO STAFF VALUES
+(12986045, 'Chang', 'Joe','AdminStaff','19/02/1986','M','21 oakview street, Northcote, Auckland','02678917','Changjoe@mailme.com','8213');
+
+INSERT INTO STAFF VALUES
+(12939260, 'Swift', 'Taylor','SeniorInstructor','12/01/1981','F','09 Blankspace street, Northcote, Auckland','02679399','taylorswift@mailme.com','8213');
+
+INSERT INTO STAFF VALUES
+(12940725, 'john', 'smith','SeniorInstructor','16/03/1989','M','14 Hobbs street, Northcote, Auckland','026783978','smithjs@mailme.com','8213');
